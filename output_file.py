29574d7bd71146f0b4b7abb87a291358c917e4c5
Ron Gatenio <ron.gatenio@gmail.com>
2022-02-24 21:13:23 +0200

    updated output file generator

--- output_file.py
+++ output_file.py
@@ -2,10 +2,15 @@ from typing import List
 from utils import ProjectSolution
 
 
-def generate_output_file(project_solutions: List[ProjectSolution]):
+def generate_output_file_data(project_solutions: List[ProjectSolution]):
     output_lines = []
     output_lines.append(str(len(project_solutions)))
     for project_solution in project_solutions:
         output_lines.append(project_solution.project.name)
         output_lines.append(' '.join(c.name for c in project_solution.contributors))
     return '\n'.join(output_lines)
+
+
+def generate_output_file(project_solutions: List[ProjectSolution], output_file_path='output.txt'):
+    with open(output_file_path, 'w') as f:
+        f.write(generate_output_file_data(project_solutions))
