778be3f39ac947ebc06f28b90cef63c2916b4b05
shiko <shayshi@camous.technion.ac.il>
2022-02-24 21:07:30 +0200

    refactored utils to support UML

--- utils.py
+++ utils.py
@@ -3,18 +3,32 @@ from typing import List, Dict, Tuple
 
 
 @dataclass
-class Project:
-    name: str
-    duration: int
-    score: int
-    best_before: int
-    skills: List[Tuple[str, int]]
-
-
-@dataclass
 class Contributor:
     name: str
     skills: Dict[str, int]
+    next_available_day: int
+
+    def get_role_dist(self):
+        """ returns distance if not relevant returns -1"""
+
+
+@dataclass
+class Role:
+    skills: Dict[str, int]
+    best_before: int
+    duration: int
+
+
+@dataclass
+class Project:
+    name: str
+    Roles: List[Role]
+    score: int
+    duration: int
+
+    def get_proj_score(self):
+        """returns score of the project"""
+        pass
 
 
 class ProjectSolution:
